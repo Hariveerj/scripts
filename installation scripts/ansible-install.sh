d197bcf0b72b37d0e6446c9a700abb56d2d69a79
hariveerj <hari.jujjuri@vibhohcm.com>
2025-01-08 01:36:27 +0530

    third

--- installation scripts/ansible-install.sh
+++ installation scripts/ansible-install.sh
@@ -1,12 +1,23 @@
-# Update the system packages 
-sudo apt-get update -y 
-# Install required software properties package 
-sudo apt-get install -y software-properties-common 
-# Add the Ansible PPA (Personal Package Archive) 
-sudo add-apt-repository --yes --update ppa:ansible/ansible 
-# Update the system packages again to include the new repository 
-sudo apt-get update -y 
-# Install Ansible 
-sudo apt-get install -y ansible 
-# Verify the installation by checking Ansible version 
+# Update the system packages
+sudo apt-get update -y
+
+# Install required software properties package
+sudo apt-get install -y software-properties-common
+
+# Add the Ansible PPA (Personal Package Archive)
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+# Update the system packages again to include the new repository
+sudo apt-get update -y
+
+# Install Ansible
+sudo apt-get install -y ansible
+
+# Install Ansible Navigator
+sudo apt-get install -y ansible-navigator
+
+# Verify the installation by checking Ansible version
 ansible --version
+
+# Verify the Ansible Navigator installation
+ansible-navigator --version
